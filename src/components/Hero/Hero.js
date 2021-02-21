16a901b133c10f320b4151886327cb7219d04d37
Szarnecki Przemysław <siarniuk@gmail.com>
2021-02-21 12:43:28 +0100

    Add Components

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+class Hero  extends React.Component {
+  render() {
+    return (
+      <header className={styles.Component}>
+          <h2 className={styles.title}>Things to do</h2>
+          <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="space" className={styles.image}></img>
+      </header>
+    )
+  }
+}
+
+export default Hero;
