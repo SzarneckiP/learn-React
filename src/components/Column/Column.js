c93710f3e9025559875275c16afca9d91dea47ba
Szarnecki Przemysław <siarniuk@gmail.com>
2021-02-23 08:29:48 +0100

    Add cart

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import { settings } from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
+import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-    static propTypes = {
-        titleColumn: PropTypes.string,
+    state = {
+        cards: this.props.cards || [],
     }
-    render(){
-        return(
+
+    static propTypes = {
+        title: PropTypes.string,
+        cards: PropTypes.array,
+        icon: PropTypes.node,
+    }
+
+    addCard(title) {
+        this.setState(state => (
+            {
+                cards: [
+                    ...state.cards,
+                    {
+                        key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+                        title,
+                    }
+                ]
+            }
+        ));
+    }
+
+    render() {
+        return (
             <section className={styles.component}>
-                <h3 className={styles.title}>{this.props.titleColumn}</h3>
+                <h3 className={styles.title}>{this.props.title}
+                    <span className={styles.icon}>
+                        <Icon name={this.props.icon} /> {/*w dataStore.js dla każdej kolumny podana jest właściwość icon, którą należy przekazać do propsa name komponentu Icon.*/}
+                    </span>
+                </h3>
+                {this.state.cards.map(({ key, ...cardsProps }) => (
+                    <Card key={key} {...cardsProps} />
+                ))}
+                <div className={styles.creator}>
+                    <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+                </div>
             </section>
         )
     }
