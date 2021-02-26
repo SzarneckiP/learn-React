b46922f23a60cf3efea064f5827e27cdc53ef72a
Szarnecki Przemysław <siarniuk@gmail.com>
2021-02-26 12:22:39 +0100

    Add Container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+const Container = props => (
+    <div className={styles.component}>{props.children}</div> //komponent funkcjonalny
+);
+
+Container.propTypes = {
+    children: PropTypes.element,
+};
+
+export default Container;
