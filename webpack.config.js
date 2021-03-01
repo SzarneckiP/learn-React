c8c593dc76b528329d9b7c7c021a188d26123c58
SzarneckiP <siarniuk@gmail.com>
2021-03-01 14:55:14 +0100

    init new comp

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ const baseConfig = () => ({
     path: path.join(__dirname, 'dist'),
     filename: 'scripts_bundle.js',
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
@@ -19,7 +22,7 @@ const baseConfig = () => ({
         },
       },
     ],
-  }, 
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -33,14 +36,14 @@ const devConfig = () => ({
       {
         test: /\.(s*)css$/,
         use: [
-          'style-loader', 
+          'style-loader',
           {
             loader: 'css-loader',
             query: {
-              modules: true, 
+              modules: true,
               localIdentName: '[name]_[local]_[hash:base64:5]',
             },
-          }, 
+          },
           'sass-loader',
         ],
       },
@@ -52,21 +55,21 @@ const prodConfig = () => ({
   module: {
     rules: [
       {
-        test:/\.(s*)css$/,
-        use:[
+        test: /\.(s*)css$/,
+        use: [
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             query: {
-              modules: true, 
+              modules: true,
               localIdentName: '[name]_[local]_[hash:base64:5]',
             },
-          }, 
+          },
           'sass-loader',
         ],
       },
     ],
-  }, 
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'styles_bundle_[name].css',
