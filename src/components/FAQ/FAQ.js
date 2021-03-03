92235b6c294446e4ff1cd36f8870d4bff114ef7f
SzarneckiP <siarniuk@gmail.com>
2021-03-03 08:26:00 +0100

    fix bugs info and FAQ pages

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Container from '../Container/Container';
 import { pageData } from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
-import styles from './FAQ.scss';
 import Hero from '../Hero/Hero';
-import PropTypes from 'prop-types';
+
 
 class FAQ extends React.Component {
 
-    static propTypes = {
-        title: PropTypes.string,
-        image: PropTypes.string,
-    }
 
     render() {
-        const { title, image } = this.props;
         return (
             <Container>
-                <Hero title={title} image={image} />
-                <header className={styles.component}>
-                    <h2 className={styles.title}>{ReactHtmlParser(pageData.title)}</h2>
-                    <img className={styles.image} src={pageData.image} alt="space"></img>
-                </header>
+                <Hero title={pageData.title} image={pageData.image} />
                 <div>
                     <h2>{pageData.faqTitle}</h2>
                     <p>{pageData.faqDescription}</p>
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Container from '../Container/Container';
-import styles from './Info.scss';
 import { pageData } from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
+import Hero from '../Hero/Hero';
+
 
 const Info = () => (
     <Container>
-        <header className={styles.component}>
-            <h2 className={styles.title}>{ReactHtmlParser(pageData.title)}</h2>
-            <img className={styles.image} src={pageData.image} alt="space"></img>
-        </header>
+        <Hero title={pageData.infoTitle} image={pageData.image} />
         <div>
             <h2>{pageData.infoTitle}</h2>
             <p>{pageData.infoDescription}</p>
